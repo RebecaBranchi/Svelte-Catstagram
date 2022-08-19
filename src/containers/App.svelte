253c6558be9da222335322cd6879da9a73b4a115
Rebeca Branchi <rebecabranchi@gmail.com>
2022-08-19 14:49:44 -0300

    first commit

---
+++ src/containers/App.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {onMount} from 'svelte';
+    import Header from "../componentes/Header.svelte";
+    import Main from "../componentes/Main.svelte";
+    import TimeLine from "../componentes/TimeLine.svelte";
+    import Sidebar from "../componentes/Sidebar.svelte";
+
+    let data ={};
+    const API = "https://kittygram-api.vercel.app/";
+
+    onMount(async()=>{
+      const response = await fetch(API);
+      data = await response.json();
+    });
+
+</script>
+
+
+
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Pacifico&display=swap");
+    @import url("https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap");
+  
+    :global(body) {
+      background-color: #fafafa;
+      color: rgba(38, 38, 38, 0.7);
+      font-family: "Lato", sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+    :global(h1, h2, h3) {
+      margin: 0;
+      padding: 0;
+    }
+  </style>
+
+<Header/>
+<Main>
+    <TimeLine posts={data.posts}/>
+    <Sidebar {...data.user}/>
+</Main>
+
+
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from '../src/containers/App.svelte';
+
+
+const app = new App({
+    target: document.querySelector('main')
+});
+
+
+
+export default app;
